eb58307486288c1dd62ef5603ac1c29a66dea3f0
Bartlomiej Bombelka <bartlomiej.bombelka_ext@autoscout24.com>
2023-01-10 12:58:48 +0100

    add pagination

--- src/api/hooks/usePostsQuery.ts
+++ src/api/hooks/usePostsQuery.ts
@@ -5,8 +5,21 @@ export const POSTS_QUERY_KEY = 'POSTS';
 
 export type Post = { id: string; title: string; body: string; likes: number; vote: number };
 
-export const usePostsQuery = () => {
-  return useQuery<Post[]>([POSTS_QUERY_KEY], () => axios.get('http://localhost:5001/posts').then((res) => res.data), {
-    refetchOnWindowFocus: false,
-  });
+export const usePostsQuery = ({ page }: { page: number }) => {
+  return useQuery<Post[]>(
+    [POSTS_QUERY_KEY, page],
+    () =>
+      axios
+        .get('http://localhost:5001/posts', {
+          params: {
+            _page: page,
+          },
+        })
+        .then((res) => res.data),
+    {
+      refetchOnWindowFocus: false,
+      // keepPreviousData: true, // this makes sure no api calls are made when changing pages
+      staleTime: 30 * 1000,
+    },
+  );
 };
--- src/app/posts/Posts.tsx
+++ src/app/posts/Posts.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
+
 import { usePostsQuery } from 'api/hooks/usePostsQuery';
 
 import { Post } from './Post';
 
 export const Posts = () => {
-  const { data, isError, isLoading } = usePostsQuery();
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data, isError, isLoading } = usePostsQuery({ page: currentPage });
 
   if (isLoading)
     return <div style={{ width: '100vw', height: '100vh', display: 'flex', margin: 'auto' }}>Loading . . . </div>;
@@ -15,6 +18,19 @@ export const Posts = () => {
       {data?.map(({ title, body, id, likes, vote }) => (
         <Post id={id} key={id} body={body} title={title} likes={likes} vote={vote} />
       ))}
+      <div>
+        <ul style={{ listStyle: 'none', display: 'flex', justifyContent: 'space-around' }}>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((page) => (
+            <li
+              key={page}
+              style={{ color: currentPage === page ? 'grey' : 'blue', cursor: 'pointer' }}
+              onClick={() => setCurrentPage(page)}
+            >
+              {page}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
